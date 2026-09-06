7893dfc964065b4766d78b54be7bcf1958b96a7f
silpa <silpa@gmail.com>
2026-09-06 07:43:29 +0000

    java file data addede

---
+++ app.java
@@ -0,0 +1,11 @@
+// Every line of code in Java must be inside a class. 
+// The class name must exactly match the filename (e.g., Main.java).
+public class Main {
+    
+    // The main method is the entry point where execution begins.
+    public static void main(String[] args) {
+        
+        // Prints the text inside quotes to the console
+        System.out.println("Hello, World!");
+    }
+}
